5b2bc70ede01a795fc2dce6f23fd7afc791700e6
Giovanna <giovannasfalcon8@gmail.com>
2025-07-16 19:13:44 -0300

    Atualização na linha 13 do Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
         <div className="flex flex-col lg:flex-row lg:items-center gap-8">
           <div className="flex gap-2 items-center lg:flex-1">
             <Logo className="h-6 w-6" />
-            <div className="font-medium">AI Startup Landing Page</div>
+            <div className="font-medium">AI Startup Landing Page - By Giovanna S. Falcon</div>
           </div>
           <nav className="flex flex-col lg:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center">
             <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">
@@ -30,7 +30,7 @@ export const Footer = () => {
             </a>
           </nav>
           <div className="flex gap-5 lg:flex-1 lg:justify-end">
-            <XSocial className="text-white/40 hover:text-white transition" />
+            <XSocial className="text-white/40 hover:text-white transition"/>
             <InstaSocial className="text-white/40 hover:text-white transition" />
             <YTSocial className="text-white/40 hover:text-white transition" />
           </div>
